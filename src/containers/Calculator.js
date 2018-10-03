41f8af3a4983dab5d5e5241ddb94463bd028b375
Kris <peklarick@]mail.com>
2018-10-03 14:48:58 +0200

    init commit

---
+++ src/containers/Calculator.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Calculator extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                {/* <Display /> */}
+                {/* <InputPad />
+                <Operations /> */}
+            </React.Fragment>
+        );
+    }
+}
+
+export default Calculator;
