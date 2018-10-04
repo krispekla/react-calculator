207728b065f710ddcc39c98cc0a044b2a9fd0874
Kris <peklarick@]mail.com>
2018-10-04 15:32:29 +0200

    style updated

--- src/components/NumberPad/NumberPad.js
+++ src/components/NumberPad/NumberPad.js
@@ -24,17 +24,28 @@ const keyArray = [
 ];
 
 const numberPad = props => {
- 
   const mappedNumberPad = keyArray.map(btn => {
-    let typeCheck=(()=>props.operation(btn.value));
-    if (!isNaN(btn.value) || btn.value ==='.') {
-       typeCheck=props.buttonSubmit;
-        
+    let typeCheck = () => props.operation(btn.value);
+    
+    let padStyle= "Button";
+    if (!isNaN(btn.value) || btn.value === ".") {
+      typeCheck = props.buttonSubmit;
     }
+    else if (btn.value==='AC' || btn.value==='+/-' || btn.value==='%') {
+      padStyle += " ButtonGrey";      
+    }
+    else {
+      padStyle += " ButtonOrange";
+    }
+
+    if (btn.value==='0') {
+      padStyle += " Zero";
+    }
+   
     return (
       <button
         key={btn.value}
-        className="Button"
+        className={padStyle}
         type="submit"
         value={btn.value}
         onClick={typeCheck}
